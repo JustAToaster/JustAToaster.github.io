6707fff625effd05c98ce0a459f374680bc15f6f
Dani <dani@iMac-di-Dani.home>
2020-07-26 12:57:47 +0200

    Deleting everything because IS is no more

--- wss.js
+++
@@ -1,8 +0,0 @@
-const WebSocket = require(‘ws’);
-const wss = new WebSocket.Server({port: 5000});
-
-wss.on(‘connection’, function connection(ws){
-	ws.on(‘message’, function incoming(message){
-	console.log(‘received: %s’, message);
-	});
-});
